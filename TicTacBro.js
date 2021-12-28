ea933b281d99bfd400115d33090484939efe07d2
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2021-12-28 19:36:49 +0000

    Push V2 Completed Minimax: Next Step test loop.

--- TicTacBro.js
+++ TicTacBro.js
@@ -1,3 +1,4 @@
+//define the variables and return statement (for the console)
 var field = {1: ' ', 2: ' ', 3: ' ',
 4: ' ', 5: ' ', 6: ' ',
 7: ' ', 8: ' ', 9: ' ',};
@@ -18,6 +19,7 @@ function display(){
     console.log(represent());
 }
 
+//various checks for the state of the game
 function draw_check(){
     for (let key in field){
         if (field[key] != 'X' && field[key] != 'O'){
@@ -35,7 +37,7 @@ function space_clear(position){
         return false;
     }
 }
-
+//The winning combinations, basic approach to checking if a winstate has occured.
 function check_win(){
     if(field[1] == field[2] && field[1] == field[3] && field[1] != ' '){
         return true;
@@ -74,4 +76,78 @@ function who_won(value){
     }else if(field[7] == field[5] && field[7] == field[3] && field[7] == value){
     return true;
     }
+}
+
+//bot and player move setting 
+function playerset(position){
+    field[position] = 'X';
+}
+
+function botset(position){
+    if(space_clear() && !check_win() && !draw_check()){
+        field[position] = 'O';
+    }
+}
+
+//minimax calculations
+function minimaxbest(){
+    var Bscore = -1000;
+    var Bmove = 0;
+    for (let key in field){
+        if (field[key] == ' '){
+            field[key] = 'O';
+            var score = minimax(0, false);
+            field[key] = ' '
+            if (score > Bscore){
+                Bscore = score;
+                Bmove = key;
+            }
+        }
+    }
+    botset(Bmove);
+    return;
+}
+
+function minimax(depth, MaxMin){
+    if(who_won('O')){
+        return 1;
+    } else if (who_won('X')) {
+        return -1;
+    } else if (draw_check()){
+        return 0;
+    }
+    if (MaxMin){
+        var Bscore = -1000;
+        for (let key in field){
+            if(field[key] == ' '){
+                field[key] = 'O';
+                var score = minimax(depth+1, false);
+                field[key] = ' ';
+                if(score > Bscore){
+                    Bscore = score;
+                }
+            }
+        }
+        return Bscore
+    }else{
+        var Bscore = 1000;
+        for (let key in field){
+            if(field[key] == ' '){
+                field[key] = 'X';
+                var score = minimax(depth+1, true);
+                field[key] = ' ';
+                if (score < Bscore){
+                    Bscore = score;
+                }
+            }
+        }
+        return Bscore
+    }
+}
+
+// testing loop
+
+function Begin(){
+    var proceed = true;
+    while()
 }
