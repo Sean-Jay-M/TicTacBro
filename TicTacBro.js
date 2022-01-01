3a4057bbd40d90665710f0966b6c090b7cd8c1a1
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-01-01 01:07:33 +0000

    Push VII: Complete. Future updates if debugging needed.

--- TicTacBro.js
+++ TicTacBro.js
@@ -1,11 +1,15 @@
 //Button management
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
-
 function positiontaken(position){
     if(field[position] == 'O'){
         console.log("Dang bro, I already played there !")
+        reset()
+        Angry()
+        typeWriter2()
     } else if (field[position] == 'X'){
         console.log ("Bruh, you already played there.")
+        reset()
+        Tired()
+        typeWriter3()
     }
 }
 
@@ -23,6 +27,28 @@ function resetboard(){
     document.getElementById("bottomleft").src = "images/bottomleft.png";
     document.getElementById("bottommiddle").src = "images/bottommiddle.png";
     document.getElementById("bottomright").src = "images/bottomright.png";
+}
+
+function changeimagePlayer(image){
+    if (image == 1){
+        document.getElementById("topleft").src = "images/topleftPlayer.png";
+    } else if (image == 2){
+        document.getElementById("topmiddle").src = "images/topmiddlePlayer.png";
+    } else if (image == 3){
+        document.getElementById("topright").src = "images/toprightPlayer.png";
+    } else if (image == 4){
+        document.getElementById("middleleft").src = "images/middleleftPlayer.png";
+    } else if (image == 5){
+        document.getElementById("middlem").src = "images/MiddlemPlayer.png";
+    } else if (image == 6){
+        document.getElementById("middleright").src = "images/middlerightPlayer.png";
+    } else if (image == 7){
+        document.getElementById("bottomleft").src = "images/bottomleftPlayer.png";
+    } else if (image == 8){
+        document.getElementById("bottommiddle").src = "images/bottommiddlePlayer.png";
+    } else if (image == 9){
+        document.getElementById("bottomright").src = "images/bottomrightPlayer.png";
+    }
 }
 
 function changeimageBot(image){
@@ -47,7 +73,190 @@ function changeimageBot(image){
     }
 }
 
+//Bot Visualizer 
+//restarter 
 
+function resetreactions(){
+    i = 0;
+    txt = ' yAy, a neW plAyer !'; 
+    speed = 50; 
+    txt3 = 'A win for Ai!';
+    i3 = 0;
+    speed3 = 50;
+    txt1 = 'you KneW it wAs tAken';
+    i1 = 0;
+    speed1 = 50; 
+    txt2 = 'i Am woRRied for humAns';
+    i2 = 0;
+    speed2 = 50; 
+    txt3 = 'A win for Ai!';
+    i3 = 0;
+    speed3 = 50; 
+    txt4 = "Bro, I could've won that";
+    i4 = 0;
+    speed4 = 50;
+    txt6 = " Yay, another game.";
+    i6 = 0;
+    speed6 = 50; 
+    txt5 = 'Dang, Nice Draw';
+    i5 = 0;
+    speed5 = 50; 
+}
+
+//start
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+var i = 0;
+var txt = ' yAy, a neW plAyer !'; 
+var speed = 50; 
+
+function reset(){
+    document.getElementById("talking").innerHTML = '';
+}
+
+async function backtonormal() {
+    await sleep(3000);
+    document.getElementById("BOT").src = "images/AIstart.png";
+}
+
+function typeWriter() {
+    AIstart()
+  if (i < txt.length) {
+    document.getElementById("talking").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function AIstart(){
+    document.getElementById("BOT").src = "images/AIHappy.png";
+    backtonormal();
+}
+
+//If space is taken 
+
+var txt1 = 'you KneW it wAs tAken';
+var i1 = 0;
+var speed1 = 50; 
+
+function typeWriter2() {
+    if (i1 < txt1.length) {
+      document.getElementById("talking").innerHTML += txt1.charAt(i1);
+      i1++;
+      setTimeout(typeWriter2, speed1);
+    }
+}
+
+function Angry(){
+    document.getElementById("BOT").src = "images/AIAngry.png";
+    backtonormal();
+}
+
+function Tired(){
+    document.getElementById("BOT").src = "images/AITired.png";
+    backtonormal();
+}
+
+var txt2 = 'i Am woRRied for humAns';
+var i2 = 0;
+var speed2 = 50; 
+
+function typeWriter3() {
+    if (i2 < txt2.length) {
+      document.getElementById("talking").innerHTML += txt2.charAt(i2);
+      i2++;
+      setTimeout(typeWriter3, speed2);
+    }
+}
+
+//If Ai Win (it always will win or draw)
+function Win(){
+    document.getElementById("BOT").src = "images/AIHappy.png";
+    backtonormal();
+}
+
+var txt3 = 'A win for Ai!';
+var i3 = 0;
+var speed3 = 50; 
+
+function typeWriter4(){
+    if (i3 < txt2.length) {
+      document.getElementById("talking").innerHTML += txt3.charAt(i3);
+      i3++;
+      setTimeout(typeWriter4, speed3);
+    }
+}
+
+//if game is restarted before end HERE !!!
+
+var restarts = 0;
+function restarttrack(){
+    if (restarts > 1 && !draw_check() && !check_win()){
+        reset();
+        sad();
+        typeWriter5();
+    } else if (restarts != 0 && (draw_check() || check_win())){
+        console.log("newgame")
+        newgame()
+    } else if (restarts == 0){
+        typeWriter()
+    }
+    restarts++
+}
+
+function sad(){
+    document.getElementById("BOT").src = "images/AISad.png";
+    backtonormal();
+    typeWriter5();
+}
+
+var txt4 = "Bro, I could've won that";
+var i4 = 0;
+var speed4 = 50; 
+
+function typeWriter5(){
+    if (i4 < txt4.length) {
+      document.getElementById("talking").innerHTML += txt4.charAt(i4);
+      i4++;
+      setTimeout(typeWriter5, speed4);
+    }
+}
+
+var txt6 = "Yay, another game.";
+var i6 = 0;
+var speed6 = 50; 
+
+function newgame(){
+    console.log("in newgame")
+    console.log(i6)
+    console.log(speed6)
+    if (i6 < txt6.length) {
+      document.getElementById("talking").innerHTML += txt6.charAt(i6);
+      i6++;
+      setTimeout(newgame, speed6);
+    }
+}
+
+//If Draw
+
+function Annoyed(){
+    document.getElementById("BOT").src = "images/AIAnnoyed.png";
+    backtonormal();
+}
+
+var txt5 = 'Dang, Nice Draw';
+var i5 = 0;
+var speed5 = 50; 
+
+function typeWriter6(){
+    if (i5 < txt5.length) {
+      document.getElementById("talking").innerHTML += txt5.charAt(i5);
+      i5++;
+      setTimeout(typeWriter6, speed5);
+    }
+}
 
 
 //define the variables and return statement (for the console)
@@ -63,11 +272,6 @@ function represent(){
     var line5 = field[7] + '|' + field[8] + '|' + field[9];
     completefield = line1 + '\n' + line2 + '\n' + line3 + '\n' + line4 + '\n' + line5;
     return completefield;
-}
-
-
-function display(){
-    document.getElementById("TicTacTester").innerHTML= represent();
 }
 
 //various checks for the state of the game
@@ -130,12 +334,19 @@ function who_won(value){
 
 //bot and player move setting 
 function playerset(position){
+    changeimagePlayer(position);
     field[position] = 'X';
     if (check_win()){
+        reset();
+        Win();
+        typeWriter4();
         console.log("Player Won !")
     }
     if (draw_check()){
         console.log("Draw!")
+        reset();
+        Annoyed();
+        typeWriter6();
     }
 }
 
@@ -143,10 +354,16 @@ function botset(position){
         changeimageBot(position);
         field[position] = 'O';
     if (check_win()){
+        reset();
+        Win();
+        typeWriter4();
         console.log("Bot     Won !")
     }
     if (draw_check()){
-        console.log("Draw!")
+        console.log("Draw!");
+        reset();
+        Annoyed();
+        typeWriter6();
     }
 }
 
@@ -207,8 +424,10 @@ function minimax(depth, MaxMin){
 
 // testing loop
 var proceed = true;
-function game(position){
-        if(!check_win() && draw_check()){
+async function game(position){
+        if(restarts == 0){
+            return;
+        } else if(!check_win() && draw_check()){
             proceed = false;
             return;
         }else if(check_win() && !draw_check()){
@@ -224,15 +443,16 @@ function game(position){
             }
             if(space_clear(position)){
                 playerset(position);
+                await sleep(500);
+                bot();
             }else{
                 console.log("Space is taken")
             }
 }
 }
 
-function bot(){
+async function bot(){
     minimaxbest();
-    document.getElementById("TicTacTester").innerHTML= represent();
     console.log(represent());
 }
 
